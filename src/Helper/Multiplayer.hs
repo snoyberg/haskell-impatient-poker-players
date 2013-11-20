11e766754973e5e3c3715b7df0255340c8fa7141
Michael Snoyman <michael@fpcomplete.com>
2013-11-20 09:40:56 +0000

    Much nicer multiplayer UI

--- src/Helper/Multiplayer.hs
+++ src/Helper/Multiplayer.hs
@@ -12,7 +12,6 @@ module Helper.Multiplayer
     , askPlayer
     , getPlayers
     , playMultiplayerGame
-    , clearMessages
     , Player
     ) where
 
@@ -27,7 +26,7 @@ import Control.Monad.Random.Class
 data GameData = GameData
     { gdPlayers :: !(Vector Player)
     , gdMessages :: !(HashMap Player (Vector Message))
-    , gdNextMessageId :: !Word32
+    , gdToken :: !Word32
     , gdRandomGen :: !StdGen
     }
 newtype Game a = Game
@@ -67,17 +66,14 @@ tellAllPlayers msg = do
 tellPlayer :: Player -> String -> Game ()
 tellPlayer player content = Game $ \gd -> do
     let msgs = fromMaybe mempty $ lookup player $ gdMessages gd
-        msg = Message (pack content) (gdNextMessageId gd)
+        msg = pack content
         gd' = gd { gdMessages = insert player (msgs ++ singleton msg) $ gdMessages gd
-                 , gdNextMessageId = succ $ gdNextMessageId gd
+                 , gdToken = gdToken gd + 1
                  }
      in (gd', NMDone ())
 
-clearMessages :: Game ()
-clearMessages = Game $ \gd -> (gd { gdMessages = mempty }, NMDone ())
-
 askPlayer :: Player -> String -> Game String
-askPlayer p s = Game $ \gd -> (gd, NMAskPlayer p s return)
+askPlayer p s = Game $ \gd -> (gd { gdToken = gdToken gd + 1 }, NMAskPlayer p s return)
 
 getPlayers :: Game [Player]
 getPlayers = Game $ \gd -> (gd, NMDone $ unpack $ gdPlayers gd)
@@ -96,10 +92,7 @@ newtype GameName = GameName { unGameName :: Text }
 newtype Player = Player { playerName :: Text }
     deriving (PathPiece, Eq, ToMarkup, Hashable, Ord)
 
-data Message = Message
-    { messageText :: !Text
-    , messageId :: !Word32
-    }
+type Message = Text
 
 instance Show Player where
     show = unpack . playerName
@@ -108,13 +101,39 @@ data GameState = GSNeedPlayers !(Vector Player)
                | GSRunning !GameData !(NextMove ())
 
 mkYesod "App" [parseRoutes|
-/ HomeR GET
 /set-player-name SetPlayerNameR GET POST
-/join-game JoinGameR GET
+/ JoinGameR GET
 /game/#GameName GameR GET POST
+/game/#GameName/token GameTokenR GET
 |]
 
-instance Yesod App
+instance Yesod App where
+    defaultLayout w = do
+        pc <- widgetToPageContent $ do
+            w
+            addStylesheetRemote "//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css"
+            addStylesheetRemote "//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap-theme.min.css"
+            addScriptRemote "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
+        app <- getYesod
+        giveUrlRenderer
+            [hamlet|
+                $doctype 5
+                <html>
+                    <head>
+                        <title>#{pageTitle pc}
+                        <meta name=viewport content="width=device-width, initial-scale=1.0">
+                        ^{pageHead pc}
+                    <body>
+                        <div .container>
+                            <h1>#{pageTitle pc}
+                            ^{pageBody pc}
+                            <footer .panel-footer>
+                                You're playing #{title app} on
+                                <a href=https://www.fpcomplete.com>FP Haskell Center#
+                                , powered by
+                                <a href=http://www.yesodweb.com>Yesod#
+                                .
+            |]
 instance RenderMessage App FormMessage where
     renderMessage _ _ = defaultFormMessage
 
@@ -127,36 +146,27 @@ withPlayerName f = do
             redirect SetPlayerNameR
         Just pn -> f $ Player pn
 
-getHomeR :: Handler Html
-getHomeR = withPlayerName $ \pn -> do
-    App {..} <- getYesod
-    defaultLayout $ do
-        setTitle $ toHtml title
-        [whamlet|
-            <h1>#{title}
-            <p>
-                Hello #{pn}. You can
-                <a href=@{JoinGameR}>join a game
-                at any time.
-        |]
-
 getSetPlayerNameR, postSetPlayerNameR :: Handler Html
 getSetPlayerNameR = do
-    ((res, form), enctype) <- runFormPost $ renderDivs $ areq playerNameField "Enter your codename"
-        { fsAttrs = [("autofocus", "autofocus")]
+    ((res, form), enctype) <- runFormPost $ renderBootstrap $ areq playerNameField "Enter your codename"
+        { fsAttrs =
+            [ ("autofocus", "autofocus")
+            , ("class", "form-control")
+            ]
         } Nothing
     case res of
         FormSuccess name -> do
             setSession "player-name" name
             setMessage $ toHtml $ "Your codename is now: " ++ name
-            redirectUltDest HomeR
+            redirectUltDest JoinGameR
         _ -> defaultLayout $ do
             setTitle "Set your name"
             [whamlet|
-                <h1>Set your name
-                <form method=post enctype=#{enctype}>
-                    ^{form}
-                    <input type=submit value="Set my name">
+                <form role=form method=post enctype=#{enctype}>
+                    <div .form-group>
+                        ^{form}
+                    <div .form-group>
+                        <button .btn .btn-default>Set my name
             |]
   where
     playerNameField = checkBool validName (asText "Your codename must be at least 3 characters long and consist of A-Z, a-z, 0-9, - and _") textField
@@ -187,7 +197,7 @@ getJoinGameR = withPlayerName $ \pn -> do
                 let (gd, nm) = unGame game GameData
                                 { gdPlayers = v'
                                 , gdMessages = mempty
-                                , gdNextMessageId = minBound
+                                , gdToken = minBound
                                 , gdRandomGen = randomGen
                                 }
                  in GSRunning gd nm
@@ -199,6 +209,45 @@ getJoinGameR = withPlayerName $ \pn -> do
         let (gameName, newGameState) = loop $ unpack gs
         writeTVar games $ insert gameName newGameState gs
         return $ redirect $ GameR gameName
+
+getGameTokenR :: GameName -> Handler Value
+getGameTokenR gn = do
+    moldTokenT <- lookupGetParam "token"
+    moldToken <-
+        case moldTokenT of
+            Nothing -> notFound
+            Just "waiting" -> return Nothing
+            Just t ->
+                case readMay $ unpack t of
+                    Nothing -> notFound
+                    Just i -> return $ Just i
+    App {..} <- getYesod
+    join $ liftIO $ atomically $ do
+        gss <- readTVar games
+        case lookup gn gss of
+            Nothing -> return notFound
+            Just GSNeedPlayers {} ->
+                case moldToken of
+                    Nothing -> retry
+                    Just _ -> return $ return $ object ["token" .= asText "waiting"]
+            Just (GSRunning gd _) ->
+                case moldToken of
+                    Just t | t == gdToken gd -> retry
+                    _ -> return $ return $ object ["token" .= gdToken gd]
+
+reloadWidget :: GameName -> Maybe Word32 -> Widget
+reloadWidget gn token = do
+    let url = (GameTokenR gn, [("token", pack $ maybe "waiting" show token)])
+    toWidget
+        [julius|
+            $(function(){
+                $.ajax({
+                    dataType: "json",
+                    url: "@?{url}",
+                    complete: function(){ location.reload() }
+                });
+            });
+        |]
 
 getGameR :: GameName -> Handler Html
 getGameR gn = withPlayerName $ \pn -> do
@@ -214,12 +263,23 @@ getGameR gn = withPlayerName $ \pn -> do
                     $forall p <- players
                         <li>#{p}
             |]
+            reloadWidget gn Nothing
         GSNeedPlayers _ -> redirect JoinGameR
         GSRunning gd nm -> defaultLayout $ do
             setTitle $ toHtml $ "Playing a game of " ++ title
+            toWidget
+                [julius|
+                    $(function(){
+                        $('html, body').animate(
+                           { scrollTop: $(document).height()-$(window).height()},
+                           1400
+                        );
+                    });
+                |]
             [whamlet|
+                <h2>Players: #{intercalate ", " $ map playerName $ gdPlayers gd}
                 <ul>
-                    $forall Message msg _ <- fromMaybe mempty $ lookup pn $ gdMessages gd
+                    $forall msg <- fromMaybe mempty $ lookup pn $ gdMessages gd
                         <li>#{msg}
             |]
             case nm of
@@ -232,7 +292,9 @@ getGameR gn = withPlayerName $ \pn -> do
                                 <input type=text name=input autofocus>
                                 <input type=submit>
                         |]
-                    | otherwise -> [whamlet|<p>Waiting for response from #{p}.|]
+                    | otherwise -> do
+                        [whamlet|<p>Waiting for response from #{p}.|]
+                        reloadWidget gn $ Just $ gdToken gd
 
 postGameR :: GameName -> Handler ()
 postGameR gn = withPlayerName $ \pn -> do
